9c86013cd76c6e55df63403e5533d53980d01853
michalkurzeja <michal.kurzeja@polcode.net>
2014-07-16 15:07:14 +0200

    Category

---
+++ src/JKP/AdminBundle/Controller/CategoryController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace JKP\AdminBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class CategoryController extends Controller
+{
+
+} 
