6b6e04a9da5f729356db60f5bc08f0474e3db066
michalkurzeja <michal.kurzeja@polcode.net>
2014-07-16 14:38:00 +0200

    FileController

---
+++ src/JKP/AdminBundle/Controller/FileController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace JKP\AdminBundle\Controller;
+
+use JKP\CoreBundle\Entity\File;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class FileController extends Controller
+{
+} 
